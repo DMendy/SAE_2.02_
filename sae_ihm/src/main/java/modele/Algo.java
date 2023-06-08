3e477620ff765183eccced5967fd3f7752e354c5
Andreadlc <129497102+Andreadlc@users.noreply.github.com>
2023-06-08 11:26:38 +0200

    Update Algo.java
    rajout des docstrings pour Algo

--- sae_ihm/src/main/java/modele/Algo.java
+++ sae_ihm/src/main/java/modele/Algo.java
@@ -4,13 +4,17 @@ import java.util.Arrays;
 
 public class Algo {
 
-
+    /**
+     * Algorithme glouton pour trouver une solution exhaustive.
+     * 
+     * @param parEnsembleQuetes Ensemble de quêtes disponibles
+     */
     public static void algoGloutonEx(EnsembleQuetes parEnsembleQuetes) {
         Chemin chChemin = new Chemin(parEnsembleQuetes);
         chChemin.setChexp(0);
         while (chChemin.getQuetesNonFaite().size() > 0) {
             for (Quete quete : parEnsembleQuetes.getTreeEnsembleQuetes()) {
-                if (quete.quetePossible(chChemin.getQuetesFaite()) == true && quete.getNumero() != 0) {
+                if (quete.quetePossible(chChemin.getQuetesFaite()) && quete.getNumero() != 0) {
                     chChemin.ajouteQueteFaite(quete);
                     chChemin.ajoutexp(quete.getExperience());
                 }
@@ -19,18 +23,22 @@ public class Algo {
         chChemin.ajouteQueteFaite(chChemin.getQueteFin());
         for (Quete quete1 : chChemin.getQuetesFaite().values()) {
             System.out.println("\n");
-            System.out.println("Se deplace en"+ Arrays.toString(quete1.getPos()));
-            System.out.println("quete "+quete1.getNumero()+": "+quete1.getIntitule());
+            System.out.println("Se déplace en " + Arrays.toString(quete1.getPos()));
+            System.out.println("Quête " + quete1.getNumero() + ": " + quete1.getIntitule());
         }
     }
 
-
+    /**
+     * Algorithme glouton pour trouver une solution efficace.
+     * 
+     * @param parEnsembleQuetes Ensemble de quêtes disponibles
+     */
     public static void algoGloutonEff(EnsembleQuetes parEnsembleQuetes) {
         Chemin chChemin = new Chemin(parEnsembleQuetes);
         chChemin.setChexp(0);
         while (!chChemin.queteFinPossibleEfficace()) {
             for (Quete quete : parEnsembleQuetes.getTreeEnsembleQuetes()) {
-                if (quete.quetePossible(chChemin.getQuetesFaite()) == true && quete.getNumero() != 0) {
+                if (quete.quetePossible(chChemin.getQuetesFaite()) && quete.getNumero() != 0) {
                     chChemin.ajoutexp(quete.getExperience());
                     chChemin.ajouteQueteFaite(quete);
                 }
@@ -39,16 +47,23 @@ public class Algo {
         chChemin.ajouteQueteFaite(chChemin.getQueteFin());
         for (Quete quete1 : chChemin.getQuetesFaite().values()) {
             System.out.println("\n");
-            System.out.println("Se deplace en"+ Arrays.toString(quete1.getPos()));
-            System.out.println("quete "+quete1.getNumero()+": "+quete1.getIntitule());
+            System.out.println("Se déplace en " + Arrays.toString(quete1.getPos()));
+            System.out.println("Quête " + quete1.getNumero() + ": " + quete1.getIntitule());
         }
     }
+
+    /**
+     * Algorithme glouton pour trouver une solution exhaustive et retourner une chaîne représentant les quêtes accomplies.
+     * 
+     * @param parEnsembleQuetes Ensemble de quêtes disponibles
+     * @return Chaîne représentant les quêtes accomplies
+     */
     public static String afficheAlgoGloutonEff(EnsembleQuetes parEnsembleQuetes) {
         Chemin chChemin = new Chemin(parEnsembleQuetes);
         chChemin.setChexp(0);
         while (!chChemin.queteFinPossibleEfficace()) {
             for (Quete quete : parEnsembleQuetes.getTreeEnsembleQuetes()) {
-                if (quete.quetePossible(chChemin.getQuetesFaite()) == true && quete.getNumero() != 0) {
+                if (quete.quetePossible(chChemin.getQuetesFaite()) && quete.getNumero() != 0) {
                     chChemin.ajoutexp(quete.getExperience());
                     chChemin.ajouteQueteFaite(quete);
                 }
@@ -57,12 +72,19 @@ public class Algo {
         chChemin.ajouteQueteFaite(chChemin.getQueteFin());
         return chChemin.getQuetesFaite().values().toString();
     }
+
+    /**
+     * Algorithme glouton pour trouver une solution efficace et retourner une chaîne représentant les quêtes accomplies.
+     * 
+     * @param parEnsembleQuetes Ensemble de quêtes disponibles
+     * @return Chaîne représentant les quêtes accomplies
+     */
     public static String afficheAlgoGloutonEx(EnsembleQuetes parEnsembleQuetes) {
         Chemin chChemin = new Chemin(parEnsembleQuetes);
         chChemin.setChexp(0);
         while (chChemin.getQuetesNonFaite().size() > 0) {
             for (Quete quete : parEnsembleQuetes.getTreeEnsembleQuetes()) {
-                if (quete.quetePossible(chChemin.getQuetesFaite()) == true && quete.getNumero() != 0) {
+                if (quete.quetePossible(chChemin.getQuetesFaite()) && quete.getNumero() != 0) {
                     chChemin.ajouteQueteFaite(quete);
                     chChemin.ajoutexp(quete.getExperience());
                 }
