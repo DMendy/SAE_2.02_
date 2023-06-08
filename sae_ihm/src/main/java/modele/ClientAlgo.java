18e42cabec68c800c9fe11141c973b3c339de48c
Andreadlc <129497102+Andreadlc@users.noreply.github.com>
2023-06-08 11:33:40 +0200

    Update ClientAlgo.java
    ajout de la docstring ClientAlgo

--- sae_ihm/src/main/java/modele/ClientAlgo.java
+++ sae_ihm/src/main/java/modele/ClientAlgo.java
@@ -1,12 +1,16 @@
 package modele;
-import java.io.File;
 
+import java.io.File;
 
 public class ClientAlgo {
     public static void main(String[] args) {
+        // Spécifiez le chemin vers votre fichier de scénario
         File f = new File("scenarios" + File.separator + "scenario_1.txt");
+        
+        // Lecture du fichier de scénario
         EnsembleQuetes scenario = LectureFichierTexte.lecture(f);
+        
+        // Affichage de la résolution du scénario avec l'algorithme glouton
         System.out.println(Algo.afficheAlgoGloutonEx(scenario));
-
     }
 }
