7f1a4a0e7ff5bafabe09d9ac086c093a980be114
Andreadlc <129497102+Andreadlc@users.noreply.github.com>
2023-06-08 11:31:36 +0200

    Update Quete.java
    ajout de la docstring Quete

--- sae_ihm/src/main/java/modele/Quete.java
+++ sae_ihm/src/main/java/modele/Quete.java
@@ -1,21 +1,24 @@
 package modele;
 
 import java.util.*;
-public class Quete implements Comparable <Quete> {
+
+public class Quete implements Comparable<Quete> {
 
     private int numero;
-    private int [] pos = new int[2] ;
-    private int [] precond = new int[4];
+    private int[] pos = new int[2];
+    private int[] precond = new int[4];
     private int duree;
     private int experience;
     private String intitule;
+
     /**
-     * transforme la quete d'une String à un objet de la classe Quete
-     * @param ligne (String): quete sur une ligne du fichier
+     * Constructeur de la classe Quete.
+     *
+     * @param ligne Ligne représentant la quête dans le fichier.
      */
-    public Quete(String ligne){
+    public Quete(String ligne) {
         Scanner scanner = new Scanner(ligne).useDelimiter("\\|");
-        while (scanner.hasNext()){
+        while (scanner.hasNext()) {
             this.numero = scanner.nextInt();
             String strpos = scanner.next();
             this.pos = extraitPos(strpos);
@@ -26,109 +29,135 @@ public class Quete implements Comparable <Quete> {
             this.intitule = scanner.next();
         }
     }
+
     /**
-     * transforme la position de la quete en tableau à 2 valeurs
-     * @param scanner (String): position de la quete dans la ligne
-     * @return tableau de longueur 2
+     * Transforme la position de la quête en tableau à 2 valeurs.
+     *
+     * @param scanner Position de la quête dans la ligne.
+     * @return Tableau de longueur 2 représentant la position.
      */
-    public int [] extraitPos(String scanner){
+    public int[] extraitPos(String scanner) {
         String precond = scanner;
-        int [] preconditions = new int [2];
-        precond = precond.replace("(","");
-        precond = precond.replace(")","");
-        precond = precond.replace(" ","");
+        int[] preconditions = new int[2];
+        precond = precond.replace("(", "");
+        precond = precond.replace(")", "");
+        precond = precond.replace(" ", "");
         Scanner scanPrecondition = new Scanner(precond).useDelimiter(",");
-        int i= 0;
-        while(scanPrecondition.hasNext()){
+        int i = 0;
+        while (scanPrecondition.hasNext()) {
             String extrait = scanPrecondition.next();
-            if(!extrait.equals(""))
-                preconditions[i]=Integer.parseInt(extrait);
+            if (!extrait.equals(""))
+                preconditions[i] = Integer.parseInt(extrait);
             i++;
         }
         return preconditions;
     }
+
     /**
-     * transforme la precondtion de la quete en tableau à 4 valeurs
-     * @param scanner (String): preconditions de la quete dans la ligne
-     * @return tableau de longueur 4
+     * Transforme les préconditions de la quête en tableau à 4 valeurs.
+     *
+     * @param scanner Préconditions de la quête dans la ligne.
+     * @return Tableau de longueur 4 représentant les préconditions.
      */
-    public int [] extraitPrecond(String scanner){
+    public int[] extraitPrecond(String scanner) {
         String precond = scanner;
-        int [] preconditions = new int[4];
-        precond = precond.replace("(","");
-        precond = precond.replace(")","");
-        precond = precond.replace(" ","");
+        int[] preconditions = new int[4];
+        precond = precond.replace("(", "");
+        precond = precond.replace(")", "");
+        precond = precond.replace(" ", "");
         Scanner scanPrecondition = new Scanner(precond).useDelimiter(",");
-        int i= 0;
-        while(scanPrecondition.hasNext()){
+        int i = 0;
+        while (scanPrecondition.hasNext()) {
             String extrait = scanPrecondition.next();
-            if(!extrait.equals(""))
-                preconditions[i]=Integer.parseInt(extrait);
+            if (!extrait.equals(""))
+                preconditions[i] = Integer.parseInt(extrait);
             i++;
         }
         return preconditions;
     }
+
     /**
-     * permet de voir si la quete peut etre faite en regardant ses préconditions et les quetes faites
-     * @param listQueteFaite: dico des quetes faite avec comme clé leur id
-     * @return true si la quete peut etre faite ou false si la quete ne peut pas etre faite
+     * Vérifie si la quête peut être réalisée en regardant ses préconditions et les quêtes déjà faites.
+     *
+     * @param listQueteFaite Dictionnaire des quêtes déjà faites avec leur ID comme clé.
+     * @return true si la quête peut être faite, false sinon.
      */
-    public boolean quetePossible(LinkedHashMap<Integer,Quete> listQueteFaite){
+    public boolean quetePossible(LinkedHashMap<Integer, Quete> listQueteFaite) {
         Set<Integer> listIDFaite = listQueteFaite.keySet();
-        return (((this.precond[0] == 0 || (listIDFaite.contains(this.precond[0]))) ||listIDFaite.contains(this.precond[1]))) && (((this.precond[2] == 0 || (listIDFaite.contains(this.precond[2]))) ||listIDFaite.contains(this.precond[3])));
+        return (((this.precond[0] == 0 || listIDFaite.contains(this.precond[0])) || listIDFaite.contains(this.precond[1])))
+                && (((this.precond[2] == 0 || listIDFaite.contains(this.precond[2])) || listIDFaite.contains(this.precond[3])));
     }
+
     /**
-     * compare les numeros deux quetes
-     * @param parquete : une quete
-     * @return la difference entre une quete et la quete en parametre
+     * Compare les numéros de deux quêtes.
+     *
+     * @param parquete Une quête.
+     * @return La différence entre la quête actuelle et la quête en paramètre.
      */
-    public int compareTo(Quete parquete){
+    public int compareTo(Quete parquete) {
         return numero - parquete.numero;
     }
+
     /**
-     * retoune la position de la quete sur la carte
-     * @return pos (int []) : position de la quete sur la carte
+     * Retourne la position de la quête sur la carte.
+     *
+     * @return Tableau de longueur 2 représentant la position de la quête.
      */
     public int[] getPos() {
         return pos;
     }
+
     /**
-     * retourne les préconditions de la quete
-     * @return precond (int []): préconditions de la quete
+     * Retourne les préconditions de la quête.
+     *
+     * @return Tableau de longueur 4 représentant les préconditions de la quête.
      */
     public int[] getPrecond() {
         return precond;
     }
+
     /**
-     * retoune l'id de la quete
-     * @return numero (int): id de la quete
+     * Retourne l'ID de la quête.
+     *
+     * @return ID de la quête.
      */
     public int getNumero() {
         return numero;
     }
+
     /**
-     *retourne la durée que prend la quete
-     * @return duree (int): la durée de la quete
+     * Retourne la durée que prend la quête.
+     *
+     * @return Durée de la quête.
      */
     public int getDuree() {
         return duree;
     }
 
+    /**
+     * Retourne l'intitulé de la quête.
+     *
+     * @return Intitulé de la quête.
+     */
     public String getIntitule() {
         return intitule;
     }
 
     /**
-     * retourne un string qui affiche les champs des quetes
-     * @return  un string qui represente la quete
+     * Retourne une chaîne de caractères représentant les champs de la quête.
+     *
+     * @return Chaîne de caractères représentant la quête.
      */
     @Override
     public String toString() {
-        return "\n"+"quete"+" "+numero+"\n"+ "Se deplace en "+Arrays.toString(pos) +"\n"+"Obtient "+experience+"xp"+"\n"+intitule;
+        return "\n" + "quete" + " " + numero + "\n" + "Se deplace en " + Arrays.toString(pos) + "\n"
+                + "Obtient " + experience + "xp" + "\n" + intitule;
     }
+
     /**
-     * retourne l'expérience donnée de la quete
-     * @return experience(int) : expérience donné de la quete
+     * Retourne l'expérience donnée par la quête.
+     *
+     * @return Expérience donnée par la quête.
      */
     public int getExperience() {
         return experience;
